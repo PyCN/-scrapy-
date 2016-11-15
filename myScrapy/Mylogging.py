d19cc8ba983e03f119363d76c0118a62ee98231b
yqh231 <442621753@qq.com>
2016-11-15 17:15:43 +0800

    新增日志文件
    新增日志文件

---
+++ myScrapy/Mylogging.py
@@ -0,0 +1,14 @@
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='debug.log',
+                filemode='w')
+
+def INFO(str):
+	logging.info(str)
+
+def warning(str):
+	logging.warning(str)
+
