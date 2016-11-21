b6379a752871de59d3822ad796332a3be9a3c101
yqh231 <442621753@qq.com>
2016-11-21 15:21:53 +0800

    完善download模块
    完善download模块

--- myScrapy/Download.py
+++ myScrapy/Download.py
@@ -4,8 +4,9 @@ import sys
  
 import requests
 
-import Mylogging
+from Mylogging import INFO,WARNING
 from Schedule import schedule
+import Config_paser
 
 def str_to_unicode(text, encoding='utf-8'):
     """
@@ -23,13 +24,73 @@ class DownloadWorker(threading.Thread):
         self.workQueue = workQueue
 
     def run(self):
+        while not self.workQueue.empty():
+            item = self.workQueue.get()
+            if not isinstance(item, list):
+                WARNING("[Download] error type, return!!")
+                return
+
+            try:
+                url = item[0]
+            except Exception:
+                WARNING("[Download] url isn't exists, return !!")
+                return
+
+            try:
+                download_type = item[1]
+            except Exception:
+                WARNING("[Download] error in download_type!!, return!!")
+                return
+
+            method = item[2]
+            filename = item[3]
+
+            if download_type == "picture":
+                self.download_picture(image_url = url, filename = filename)
+            elif download_type == "anything":
+                self.download_anything(image_url = url, filename = filename)
+            elif download_type == "custom":
+                pass
+
+    def download_anything(self, down_url, path = "", filename = ""):
+        if path is None:
+            if Config_paser.locate is None:
+                path = os.path.getcwd()
+            else:
+                path = Config_paser.locate
+
+        INFO("[Download] [download_anthing] the file will be downloaded in {}".format(path))
+
+        if filename is not None:
+            file = filename.split('.')
+            if file[1] is None:
+                filename += '.dat'
+        else:
+            filename = down_url + '.dat'
+
+        os.path.join(path, filename)    
+
+        try:
+            result = requests.get(down_url, stream = True)
+            with open(path, 'wb') as things:
+                things.write(result.content)
+        except Exception as e:
+            WARNING(e)
 
 
-    def download_picture(self, image_url, path = ""):
-    	if(not path):
-    		path = os.getcwd()
+    def download_picture(self, image_url, path = "", filename = ""):
+    	if path is None:
+    		if Config_paser.locate is None:
+                path = os.path.getcwd()
+            else:
+                path = Config_paser.locate
 
-        os.path.join(path, '{}.jpg'.format(image_url))
+        INFO("[Download] the picture will be downloaded in {}".format(path))
+
+        if filename is not None:
+            os.path.join(path, '{}.jpg'.format(filename))
+        else:
+            os.path.join(path, '{}.jpg'.format(image_url))
 
     	try:
             image = requests.get(image_url, stream = True)
@@ -55,10 +116,17 @@ class Download(object):
     		item.start()
 
     def GetTodown(self):
-    	list = schedule.GetToDownload()
-    	while (not list.empty()):
-    		item = list.pop()
+    	items = schedule.GetToDownload()
+    	while not len(items):
+    		item = items.pop()
     		self.workQueue.put(item)
+
+    def waitForallThreadcompelete(self):
+        while len(self.workers):
+            worker = self.workers.pop()
+            worker.join()
+            if worker.isAlive() and not len(self.workers):
+                self.workers.append(worker)
 
 
 
