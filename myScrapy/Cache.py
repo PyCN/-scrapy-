a5686a5a90d8e2d67e9b628f79d636b1e1c1cd7d
yqh231 <442621753@qq.com>
2017-01-20 11:43:09 +0800

    回复缓冲区

---
+++ myScrapy/Cache.py
@@ -0,0 +1,26 @@
+class Cache(object):
+    Response_Cache = []
+
+    def __init__(self):
+        pass
+    @property
+    def cache(self):
+        if 0 == len(Response_Cache):
+            raise EnvironmentError('cache is empty!!')
+
+        return Response_Cache
+
+    @cache.setter
+    def cache(self, response):
+        if not isinstance(response, str):
+            raise ValueError('response is not string type')
+        Response_Cache.append(response)
+
+
+class Trans(object):
+    __slots__ = ('url', 'method', 'cache')
+
+    def __init__(self, **kwargs):
+        self.url = kwargs.pop('url')
+        self.method = kwargs.pop('method')
+        self.cache = kwargs.pop('cache_obj')
