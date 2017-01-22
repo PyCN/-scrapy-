28a191885acbe5af067fc0ca7a91101c72d77e9c
yqh231 <442621753@qq.com>
2017-01-22 12:03:26 +0800

    test case

--- myScrapy/src/MyScrapy.py
+++ myScrapy/src/MyScrapy.py
@@ -1,13 +1,19 @@
 from myScrapy import Myscrapy
 from myScrapy import Request
 
-app = Myscrapy(__name__)
 
+app = Myscrapy(__name__)
 class test(object):
     def start_request(self):
-        yield Request(url = 'http://www.baidu.com/', method = 'GET', callback = self.deal_response)
+        for i in xrange(1, 5):
+            yield Request(url = 'http://www.baidu.com/', method = 'GET', callback = self.deal_response)
 
     def deal_response(self, response):
+        for i in xrange(1, 5):
+            yield Request(url = 'http://www.baidu.com/', method = 'GET', callback = self.deal_response1)
+
+    def deal_response1(self, response):
         print response.xpath("//head/title")[0].text
+
 
 app.run(execute_app = test())
