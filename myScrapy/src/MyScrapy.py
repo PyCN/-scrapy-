6f87c1e7fb8525452093b1114a58d1190d5c03df
yqh231 <442621753@qq.com>
2017-01-29 11:35:32 +0800

    cookie

--- myScrapy/src/MyScrapy.py
+++ myScrapy/src/MyScrapy.py
@@ -6,14 +6,15 @@ app = Myscrapy(__name__)
 class test(object):
     def start_request(self):
         for i in xrange(1, 5):
-            yield Request(url = 'http://www.baidu.com/', method = 'GET', callback = self.deal_response)
+            yield Request(url = 'http://www.baidu.com/', method = 'GET', callback = self.deal_response, meta = {'cookieJar':1})
 
     def deal_response(self, response):
         for i in xrange(1, 5):
             yield Request(url = 'http://www.baidu.com/', method = 'GET', callback = self.deal_response1)
+            print response._cookie
 
     def deal_response1(self, response):
-        print response.xpath("//head/title")[0].text
+        print response._response_string.xpath("//head/title")[0].text
 
 
 app.run(execute_app = test())
